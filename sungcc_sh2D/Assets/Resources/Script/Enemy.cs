87b09dea5d72f576e59cda6000c0f013f5eeba52
akiyama <akidev2344@gmail.com>
2018-01-27 12:31:21 +0900

    Unityのデータをアップロード

---
+++ sungcc_sh2D/Assets/Resources/Script/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject HitParticle;
+
+	// Use this for initialization
+	void Start () {
+        HitParticle = Resources.Load<GameObject>("Prefab/HitParticle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("click");
+        Instantiate(HitParticle, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ sungcc_sh2D/Assets/Resources/Script/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject obj;
+    public Transform top;
+    public Transform bottom;
+    public float rate;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (obj == null)
+            return;
+		if(Random.value < rate)
+        {
+            Vector3 pos;
+            pos.x = Mathf.Lerp(top.position.x, bottom.position.x, Random.value);
+            pos.y = Mathf.Lerp(top.position.y, bottom.position.y, Random.value);
+            pos.z = Mathf.Lerp(top.position.z, bottom.position.z, Random.value);
+            Instantiate(obj, pos, Quaternion.identity);
+        }
+	}
+}
